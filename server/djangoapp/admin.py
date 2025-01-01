49d88732b3afa81d3a49f3772a002ad1f5f79cfd
archbaer <pereirakebaili@gmail.com>
2025-01-01 07:56:18 -0500

    lint changes

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,19 +1,23 @@
 from django.contrib import admin
 from .models import CarMake, CarModel
 
+
 class CarModelInline(admin.TabularInline):  # Inline for CarModel in CarMakeAdmin
     model = CarModel
     extra = 1
+
 
 class CarModelAdmin(admin.ModelAdmin):  # Admin for CarModel
     list_display = ('name', 'car_make', 'type', 'year')
     list_filter = ('type', 'year', 'car_make')
     search_fields = ('name',)
 
+
 class CarMakeAdmin(admin.ModelAdmin):  # Admin for CarMake with CarModelInline
     inlines = [CarModelInline]
     list_display = ('name', 'description')
     search_fields = ('name',)
+
 
 # Register models with admin site
 admin.site.register(CarMake, CarMakeAdmin)
--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,9 +1,6 @@
-# Uncomment the following imports before adding the Model code
-
 from django.db import models
-from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
-from django.contrib import admin
+
 
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
@@ -11,6 +8,7 @@ class CarMake(models.Model):
 
     def __str__(self):
         return self.name
+
 
 class CarModel(models.Model):
     car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
@@ -25,12 +23,13 @@ class CarModel(models.Model):
     ]
 
     type = models.CharField(max_length=10, choices=CAR_TYPES, default='Hatchback')
-    year = models.IntegerField(default=2023, 
+    year = models.IntegerField(
+        default=2023,
         validators=[
             MaxValueValidator(2023),
-            MinValueValidator(2015)
-        ])
+            MinValueValidator(2015),
+        ],
+    )
 
     def __str__(self):
         return self.name
-
