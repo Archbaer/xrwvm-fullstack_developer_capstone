2c1f34aac628a09eae03d5494e948f91f6f72e02
Archbaer <140851415+Archbaer@users.noreply.github.com>
2025-01-02 21:22:13 +0000

    Update populate.py

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -1,55 +1,54 @@
-from django.core.management.base import BaseCommand
-from django.db import IntegrityError
-from dan.models import CarMake, CarModel
+from .models import CarMake, CarModel
 
 
-class Command(BaseCommand):
-    help = 'Populates the CarMake and CarModel models with sample data'
+def initiate():
+    car_make_data = [
+        {"name": "NISSAN", "description": "Great cars. Japanese technology"},
+        {"name": "Mercedes", "description": "Great cars. German technology"},
+        {"name": "Audi", "description": "Great cars. German technology"},
+        {"name": "Kia", "description": "Great cars. Korean technology"},
+        {"name": "Toyota", "description": "Great cars. Japanese technology"},
+    ]
 
-    def handle(self, *args, **options):
-        car_makes = [
-            {'name': 'Toyota', 'description': 'Reliable and fuel-efficient vehicles'},
-            {'name': 'Ford', 'description': 'Innovative American cars'},
-            {'name': 'BMW', 'description': 'Luxury vehicles from Germany'},
-            {'name': 'Honda', 'description': 'Affordable and durable cars'},
-            {'name': 'Mercedes', 'description': 'Premium German automobiles'},
-            {'name': 'Chevrolet', 'description': 'Wide range of American cars'},
-        ]
+    car_make_instances = []
+    for data in car_make_data:
+        car_make_instances.append(CarMake.objects.create(
+            name=data['name'], description=data['description']))
 
-        car_models = [
-            {'name': 'Corolla', 'car_make': 'Toyota', 'type': 'SEDAN', 'year': 2023},
-            {'name': 'Camry', 'car_make': 'Toyota', 'type': 'SEDAN', 'year': 2022},
-            {'name': 'F-150', 'car_make': 'Ford', 'type': 'SUV', 'year': 2023},
-            {'name': 'Mustang', 'car_make': 'Ford', 'type': 'COUPE', 'year': 2021},
-            {'name': 'X5', 'car_make': 'BMW', 'type': 'SUV', 'year': 2022},
-            {'name': '3 Series', 'car_make': 'BMW', 'type': 'SEDAN', 'year': 2023},
-            {'name': 'Civic', 'car_make': 'Honda', 'type': 'SEDAN', 'year': 2023},
-            {'name': 'Accord', 'car_make': 'Honda', 'type': 'SEDAN', 'year': 2021},
-            {'name': 'C-Class', 'car_make': 'Mercedes', 'type': 'SEDAN', 'year': 2022},
-            {'name': 'S-Class', 'car_make': 'Mercedes', 'type': 'SEDAN', 'year': 2023},
-            {'name': 'Equinox', 'car_make': 'Chevrolet', 'type': 'SUV', 'year': 2022},
-            {'name': 'Malibu', 'car_make': 'Chevrolet', 'type': 'SEDAN', 'year': 2021},
-        ]
+    # Create CarModel instances with the corresponding CarMake instances
+    car_model_data = [
+      {"name": "Pathfinder", "type": "SUV", "year": 2023,
+       "car_make": car_make_instances[0]},
+      {"name": "Qashqai", "type": "SUV", "year": 2023,
+       "car_make": car_make_instances[0]},
+      {"name": "XTRAIL", "type": "SUV", "year": 2023,
+       "car_make": car_make_instances[0]},
+      {"name": "A-Class", "type": "SUV", "year": 2023,
+       "car_make": car_make_instances[1]},
+      {"name": "C-Class", "type": "SUV", "year": 2023,
+       "car_make": car_make_instances[1]},
+      {"name": "E-Class", "type": "SUV", "year": 2023,
+       "car_make": car_make_instances[1]},
+      {"name": "A4", "type": "SUV", "year": 2023,
+       "car_make": car_make_instances[2]},
+      {"name": "A5", "type": "SUV", "year": 2023,
+       "car_make": car_make_instances[2]},
+      {"name": "A6", "type": "SUV", "year": 2023,
+       "car_make": car_make_instances[2]},
+      {"name": "Sorrento", "type": "SUV", "year": 2023,
+       "car_make": car_make_instances[3]},
+      {"name": "Carnival", "type": "SUV", "year": 2023,
+       "car_make": car_make_instances[3]},
+      {"name": "Cerato", "type": "Sedan", "year": 2023,
+       "car_make": car_make_instances[3]},
+      {"name": "Corolla", "type": "Sedan", "year": 2023,
+       "car_make": car_make_instances[4]},
+      {"name": "Camry", "type": "Sedan", "year": 2023,
+       "car_make": car_make_instances[4]},
+      {"name": "Kluger", "type": "SUV", "year": 2023,
+       "car_make": car_make_instances[4]},
+    ]
 
-        for car_make in car_makes:
-            try:
-                make, created = CarMake.objects.get_or_create(
-                    name=car_make['name'], defaults={'description': car_make['description']}
-                )
-                if created:
-                    self.stdout.write(self.style.SUCCESS(f"Created car make: {make.name}"))
-            except IntegrityError:
-                self.stdout.write(self.style.ERROR(f"Car make {car_make['name']} already exists"))
-
-        for car_model in car_models:
-            try:
-                make = CarMake.objects.get(name=car_model['car_make'])
-                model, created = CarModel.objects.get_or_create(
-                    name=car_model['name'],
-                    car_make=make,
-                    defaults={'type': car_model['type'], 'year': car_model['year']}
-                )
-                if created:
-                    self.stdout.write(self.style.SUCCESS(f"Created car model: {model.name}"))
-            except CarMake.DoesNotExist:
-                self.stdout.write(self.style.ERROR(f"Car make {car_model['car_make']} not found"))
+    for data in car_model_data:
+        CarModel.objects.create(name=data['name'], car_make=data['car_make'],
+                                type=data['type'], year=data['year'])
